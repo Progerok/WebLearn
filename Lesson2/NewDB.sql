24483da84b6bbbf7d3baa4eeb3cccc8526c610bb
Progerok <144642636+Progerok@users.noreply.github.com>
2023-09-26 09:29:51 +0700

    Update NewDB.sql

--- Lesson2/NewDB.sql
+++ Lesson2/NewDB.sql
@@ -50,27 +50,31 @@ JOIN Lessons ON Grades.lesson_id = Lessons.id;
 -- вставляем значения
 
 INSERT INTO web2.university ("id", "uni_name") VALUES (1, 'КГПИ');
-INSERT INTO web2.university ("id", "uni_name") VALUES (3, 'НФИ');
-INSERT INTO web2.university ("id", "uni_name") VALUES (6, 'ХТИ');
-INSERT INTO web2.university ("id", "uni_name") VALUES (10, 'НЕАХТИ');
-INSERT INTO web2.university ("id", "uni_name") VALUES (15, 'АЙТЫ');
+INSERT INTO web2.university ("id", "uni_name") VALUES (2, 'НФИ');
+INSERT INTO web2.university ("id", "uni_name") VALUES (3, 'ХТИ');
+INSERT INTO web2.university ("id", "uni_name") VALUES (4, 'НЕАХТИ');
+INSERT INTO web2.university ("id", "uni_name") VALUES (5, 'АЙТЫ');
 
 INSERT INTO web2.groups ("id", "group_name", "university_id") VALUES (1, 'ММ1', 1);
-INSERT INTO web2.groups ("id", "group_name", "university_id") VALUES (3, 'АСТП', 3);
-INSERT INTO web2.groups ("id", "group_name", "university_id") VALUES (6, 'ЮВЫ', 1);
+INSERT INTO web2.groups ("id", "group_name", "university_id") VALUES (2, 'АСТП', 3);
+INSERT INTO web2.groups ("id", "group_name", "university_id") VALUES (3, 'ЮВЫ', 1);
 
 INSERT INTO web2.lessons ("id", "lesson_name") VALUES (1, 'Математика');
-INSERT INTO web2.lessons ("id", "lesson_name") VALUES (3, 'История');
-INSERT INTO web2.lessons ("id", "lesson_name") VALUES (6, 'ФунКан');
-INSERT INTO web2.lessons ("id", "lesson_name") VALUES (10, 'Дискретка');
-INSERT INTO web2.lessons ("id", "lesson_name") VALUES (15, 'Web');
-INSERT INTO web2.lessons ("id", "lesson_name") VALUES (21, 'ООП');
+INSERT INTO web2.lessons ("id", "lesson_name") VALUES (2, 'История');
+INSERT INTO web2.lessons ("id", "lesson_name") VALUES (3, 'ФунКан');
+INSERT INTO web2.lessons ("id", "lesson_name") VALUES (4, 'Дискретка');
+INSERT INTO web2.lessons ("id", "lesson_name") VALUES (5, 'Web');
+INSERT INTO web2.lessons ("id", "lesson_name") VALUES (6, 'ООП');
 
-INSERT INTO web2.students ("id", "fio", "born", "id_group", "start_year") VALUES (3, 'ИВанов И.И.', '2003-09-26', 1, 2021);
-INSERT INTO web2.students ("id", "fio", "born", "id_group", "start_year") VALUES (7, 'Петров П.П.', '2004-04-26', 1, 2021);
+INSERT INTO web2.students ("id", "fio", "born", "id_group", "start_year") VALUES (1, 'ИВанов И.И.', '2003-09-26', 1, 2021);
+INSERT INTO web2.students ("id", "fio", "born", "id_group", "start_year") VALUES (2, 'Петров П.П.', '2004-04-26', 1, 2021);
+INSERT INTO web2.students ("id", "fio", "born", "id_group", "start_year") VALUES (3, 'Федоров Ф.А.', '2004-04-26', 1, 2021);
 
-INSERT INTO web2.grades ("id", "student_id", "lesson_id", "grade") VALUES (1, 3, 15, 5);
-INSERT INTO web2.grades ("id", "student_id", "lesson_id", "grade") VALUES (3, 3, 10, 4);
-INSERT INTO web2.grades ("id", "student_id", "lesson_id", "grade") VALUES (6, 7, 15, 4);
-INSERT INTO web2.grades ("id", "student_id", "lesson_id", "grade") VALUES (10, 3, 3, 3);
-INSERT INTO web2.grades ("id", "student_id", "lesson_id", "grade") VALUES (15, 7, 1, 5);
+INSERT INTO web2.grades ("id", "student_id", "lesson_id", "grade") VALUES (1, 1, 5, 5);
+INSERT INTO web2.grades ("id", "student_id", "lesson_id", "grade") VALUES (2, 2, 4, 4);
+INSERT INTO web2.grades ("id", "student_id", "lesson_id", "grade") VALUES (3, 2, 5, 4);
+INSERT INTO web2.grades ("id", "student_id", "lesson_id", "grade") VALUES (4, 1, 2, 3);
+INSERT INTO web2.grades ("id", "student_id", "lesson_id", "grade") VALUES (5, 2, 1, 5);
+INSERT INTO web2.grades ("id", "student_id", "lesson_id", "grade") VALUES (6, 3, 1, 4);
+INSERT INTO web2.grades ("id", "student_id", "lesson_id", "grade") VALUES (7, 3, 5, 3);
+INSERT INTO web2.grades ("id", "student_id", "lesson_id", "grade") VALUES (8, 3, 6, 4);
